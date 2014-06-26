36dff5116e4182109d45f0732a6f1b233ff761bf
Ashley McKemie <ashley.mckemie@gmail.com>
2014-06-26 09:55:19 -0500

    Refactor quest characters db methods

--- lib/wwtd/database/methods/quest_characters_db.rb
+++ lib/wwtd/database/methods/quest_characters_db.rb
@@ -6,18 +6,15 @@ module WWTD
 
     # could be done by creating player /char association, finding all chars for player, then using .where to find quests? Which is faster?
     def get_players_quest_characters(player_id, quest_id)
-      result = []
       chars = QuestCharacter.where('player_id = ? AND quest_id = ?', player_id, quest_id)
-      chars.each do |character|
+      chars.map { |character|
         char = Character.find(character.character_id)
-        result << build_character(char)
-      end
-      result
+        build_character(char)
+      }
     end
 
     def delete_quest_character(player_id, character_id)
       ar_quest_character = QuestCharacter.where('player_id = ? AND character_id = ?', player_id, character_id).first
-      # binding.pry
       test_id = ar_quest_character.id
       ar_quest_character.destroy
       return true if !QuestCharacter.exists?(test_id)
