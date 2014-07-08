645c0bf685329a13ec9aa71d6798f25f15796c0b
Ashley McKemie <ashley.mckemie@gmail.com>
2014-07-07 22:19:41 -0500

    Remove unnecessary command files

--- lib/wwtd/commands/add_item_to_person.rb
+++
@@ -1,9 +0,0 @@
-module WWTD
-  # Checks to see whether it is an actual item and in that room and can be taken (has take in actions)
-  class AddItemToPerson < Command
-    def run(input)
-
-    end
-  end
-end
-
